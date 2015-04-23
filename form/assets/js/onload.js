3735a7dee48a8b85385f83059264cecad95d2554
Mike Plant <msplant@gmail.com>
2015-04-23 11:33:10 -0400

    add form events

--- form/assets/js/onload.js
+++ form/assets/js/onload.js
@@ -3,15 +3,17 @@ jQuery(function($){
     $('#form-demo')
         .validator()
         .on('submit', function (e) { // standard event
-          if (e.isDefaultPrevented()) {
-            // handle the invalid form...
-            console.log('invalid');
-          } else {
-            // everything looks good!
-            console.log('go for it');
+          if (!e.isDefaultPrevented()) {
+            // trigger custom event instead of direct call rule
+            $(e.target).trigger('form submit',{name:'DTM Form Demo'});
           }
         })
-        .on('invalid.bs.validator', function (e) { // custom event
-            console.log(e);
-        })
+        .on('invalid.bs.validator', function (e) { // custom event from plugin
+            var fieldName = $(e.relatedTarget).attr('name'),
+                message = e.detail[0],
+                obj = {name:fieldName,message:message};
+            // trigger custom event
+            console.log(e.currentTarget,obj);
+            $(e.currentTarget).trigger('invalid form submit',[{test:'test'}}]);
+        });
 });
