5c93306a437900b199b21a0ff3192c9af8ebf239
Mike Plant <msplant@gmail.com>
2015-04-23 12:22:07 -0400

    demo v1

--- form/assets/js/onload.js
+++ form/assets/js/onload.js
@@ -1,19 +1,18 @@
-jQuery(function($){
+$( document ).ready(function() {
     // standard event
     $('#form-demo')
         .validator()
         .on('submit', function (e) { // standard event
           if (!e.isDefaultPrevented()) {
             // trigger custom event instead of direct call rule
-            $(e.target).trigger('form submit',{name:'DTM Form Demo'});
+            $(e.target).trigger('form-submit',['DTM Form Demo']);
           }
         })
         .on('invalid.bs.validator', function (e) { // custom event from plugin
             var fieldName = $(e.relatedTarget).attr('name'),
                 message = e.detail[0],
                 obj = {name:fieldName,message:message};
-            // trigger custom event
-            console.log(e.currentTarget,obj);
-            $(e.currentTarget).trigger('invalid form submit',[{test:'test'}]);
+            // trigger custom event - passing object
+            $(e.target).trigger('form-invalid',obj);
         });
 });
